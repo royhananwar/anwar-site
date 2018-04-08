67a9d8fed1d2e9205205be31dfd319658a6d8190
Royhan Anwar <anwarroyhan@gmail.com>
2018-04-08 21:35:09 +0700

    setting database server

--- src/app.py
+++ src/app.py
@@ -1,8 +1,21 @@
 from flask import Flask
 
+from models import db
 
 app = Flask(__name__)
 
+POSTGRES = {
+    'user': 'MY USER',
+    'pw': 'MY PASSWORD',
+    'db': 'MY DB',
+    'host': 'MY HOST',
+    'port': '5432',
+}
 
+db_url = 'postgresql://{0}:{1}@{2}:{3}/{4}'.format(POSTGRES['user'], POSTGRES['pw'], POSTGRES['host'], POSTGRES['port'], POSTGRES['db'])
 
 app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = db_url
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
