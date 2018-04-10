9b48bdbe012a28160816fa83e4272cc0f611635e
Royhan Anwar <anwarroyhan@gmail.com>
2018-04-10 19:29:43 +0700

    add detail and update for post

--- src/route.py
+++ src/route.py
@@ -46,6 +46,7 @@ def update_category(id):
         category = Category.query.get(id)
         return render_template('category/update.html', category=category)
 
+
 # list all post
 @app.route('/post/')
 def list_post():
@@ -53,6 +54,14 @@ def list_post():
     if posts is None:
         return render_template('post/index.html', data="No post Available")
     return render_template('post/index.html', posts=posts)
+
+# get detail post
+@app.route('/detail_post/<int:id>')
+def detail_post(id):
+    post = Post.query.get(id)
+    # category = Category.query.get()
+    return render_template('post/detail.html', post=post)
+
 
 # create new post
 @app.route('/create_post/', methods=['POST', 'GET'])
@@ -69,6 +78,27 @@ def create_post():
     else:
         categories = Category.query.all()
         return render_template('post/create.html', categories=categories)
+
+@app.route('/update_post/<int:id>', methods=['POST', 'GET'])
+def update_post(id):
+    if request.method == 'POST':
+        title = request.form['post_title']
+        body = request.form['post_body']
+        category_id = request.form['category_id']
+
+        post = Post.query.get(id)
+        post.title = title
+        post.body = body
+        post.category_id = category_id
+
+        db.session.commit()
+        return redirect(url_for('list_post'))
+    else:
+        post = Post.query.get(id)
+        category = Category.query.get(post.category_id)
+        categories = Category.query.all()
+
+        return render_template('post/update.html', post=post, category=category, categories=categories)
 
 if __name__ == '__main__':
     app.run(debug=True)
