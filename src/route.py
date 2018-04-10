a60d9795f436a97068fa5f1213aee7aca82e4b7e
Royhan Anwar <anwarroyhan@gmail.com>
2018-04-10 07:11:51 +0700

    add index and create for post

--- src/route.py
+++ src/route.py
@@ -1,7 +1,7 @@
 from flask import render_template, redirect, request, url_for
 
 from app import app
-from models import db, Category
+from models import db, Category, Post
 
 # index
 @app.route('/')
@@ -23,9 +23,33 @@ def create_category():
         category = Category(name=category_name)
         db.session.add(category)
         db.session.commit()
-        return redirect(url_for('homepage'))
+        return redirect(url_for('list_category'))
     else:
         return render_template('category/create.html')
+
+# list all post
+@app.route('/post/')
+def list_post():
+    posts = Post.query.all()
+    if posts is None:
+        return render_template('post/index.html', data="No post Available")
+    return render_template('post/index.html', posts=posts)
+
+# create new post
+@app.route('/create_post/', methods=['POST', 'GET'])
+def create_post():
+    if request.method == 'POST':
+        title = request.form['post_title']
+        body = request.form['post_body']
+        category_id = request.form['category_id']
+
+        post = Post(title=title, body=body, category_id=category_id)
+        db.session.add(post)
+        db.session.commit()
+        return redirect(url_for('list_post'))
+    else:
+        categories = Category.query.all()
+        return render_template('post/create.html', categories=categories)
 
 if __name__ == '__main__':
     app.run(debug=True)
